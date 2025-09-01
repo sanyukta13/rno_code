ae2dc158b4ab6f9e6c87dacca3a717d510f2455b
sanyukta13 <sanyukta@icecube.wisc.edu>
2025-09-01 16:04:57 -0500

    Add .gitignore for Python files

--- utils/getting_runs.py
+++ utils/getting_runs.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import os
 import numpy as np
+import re
 
 def get_df(file_path):
     """
@@ -37,10 +38,41 @@ def get_runs(df, save_dir, pulser_type):
             os.makedirs(dir_path)
         os.chdir(dir_path)
         print(f'copying into {os.getcwd()}')
-        os.system(f"rnogcopy run {st} {run} --server="chicago"")
-    
+        os.system(f"rnogcopy run {st} {run}")
+
+def add_attns(df, source_dir):
+    """
+    Adds attenuation values to the dataframe
+    Parameters:
+    df (pd.DataFrame): dataframe containing the data from the csv file
+    source_dir (str): directory containing the run files
+    Returns:
+    df (pd.DataFrame): dataframe with attenuation values added
+    """
+    df['attenuation'] = 0
+    for row in df.itertuples():
+        st = row.station
+        run = row.run
+        text_file = f'{source_dir}/station{st}/run{run}/aux/comment.txt'
+        if not os.path.exists(text_file):
+            print(f"{text_file} does not exist, setting attenuation to 0")
+        else:
+            try:
+                with open(text_file, 'r') as file:
+                    content = file.read()
+                    # Use regex to extract the attenuation value
+                    match = re.search(r'(\d+)dB attenuation', content)
+                    if match:
+                        df.at[row.Index, 'attenuation'] = int(match.group(1))
+                    else:
+                        print(f"No attenuation value found in {text_file}, setting to 0")
+            except Exception as e:
+                print(f"Error reading {text_file}: {e}")
+    return df
 
 # save_dir = '/data/user/sanyukta/rno_data/cal_pulser'
-# df = get_df("quality_check__in-situ_pulsing_run_information.csv")
+source_dir = '/data/desy-mirror/inbox'
+df = get_df("quality_check__in-situ_pulsing_run_information.csv")
+add_attns(df, source_dir).to_csv("att_info.csv", index=False)
 # get_runs(df, save_dir, 'fiber0')
 # get_runs(df, save_dir, 'fiber1')
