425edfc13fe04688eba663d0c91a5dc0b2186582
sanyukta13 <sanyukta@icecube.wisc.edu>
2025-11-06 23:42:31 -0600

    cleaned tribo notebook and added cluster processing scripts

---
+++ triboelectric/cluster/process_root.py
@@ -0,0 +1,25 @@
+import sys, os, argparse
+from datetime import datetime, timezone
+sys.path.append(os.path.abspath('/data/user/sanyukta/rno_code'))
+from reading.data_reading import *
+from functions.functions import *
+import warnings
+warnings.filterwarnings("ignore")
+mattak_kwargs = {
+    "backend" : "uproot",
+    # "backend" : "pyroot"
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_path', type=str, required=True, help='Path to the root files to process')
+args = parser.parse_args()
+file = args.input_path
+
+savefile = 1  # set to 1 to save output nur file
+output_filename = file.split('/')[-1].split('.root')[0] + '.nur'
+output = os.path.join('/data/user/sanyukta/rno_code/triboelectric/nur_files', output_filename)
+selectors = [lambda event_info : event_info.triggerType != "FORCE"] #remove forced trigger events
+
+# reader = basic_read_root(file, selectors=selectors, mattak_kwargs=mattak_kwargs)
+print(f"Processing file: {file}, output will be saved to: {output}")
+# events, times, volts = get_eventsvoltstraces(reader, savefile=savefile, filename=output)
